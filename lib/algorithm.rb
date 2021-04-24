06b171c40fce39c70a1cd8adba2cbef904060e31
JoannaCoPo <jococopo@gmail.com>
2021-04-23 20:30:21 -0600

    Create key and date

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -1,13 +1,29 @@
 class Algorithm
-  attr_reader :key,
+  attr_reader :alphabet,
+              :key,
               :date
 
   def initialize
+    @alphabet = nil
     @key = nil
     @date = nil
   end
 
+  def create_alphabet
+    @alphabet = ("a".."z").to_a << " "
+  end
 
+  def create_key
+    @key = numbers.sample(5)
+  end
+
+  def numbers #find a better way for this
+    [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def create_date #date thing, Megan - IC
+    Time.new.strftime("%d%m%y").to_s
+  end
 end
 
 
--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -10,9 +10,32 @@ RSpec.describe Algorithm do
 
   it 'has attributes' do
     algorithm = Algorithm.new
+    expect(algorithm.alphabet).to eq(nil)
     expect(algorithm.key).to eq(nil)
     expect(algorithm.date).to eq(nil)
   end
 
+  it 'can create alphabet' do
+    algorithm = Algorithm.new
+    algorithm.create_alphabet
+    expect(algorithm.alphabet.length).to eq(27)
+  end
 
+  it 'has a collection of numbers' do #think of better name
+    algorithm = Algorithm.new         #is this an attribute
+    expected = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(algorithm.numbers).to eq(expected)
+  end
+
+  it 'can create a key' do
+    algorithm = Algorithm.new
+    algorithm.create_key
+    expect(algorithm.key.length).to eq(5)
+  end
+
+  it 'can create a date' do
+    algorithm = Algorithm.new
+    date = algorithm.create_date
+    expect(date).to eq("230421")
+  end
 end
