9ddcba1a24d921c3edd9a9c9962bdb78b8e5d827
JoannaCoPo <jococopo@gmail.com>
2021-04-24 22:51:57 -0600

    Store key used in encryption

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,28 +1,33 @@
 require 'date'
 class Shift
-  attr_reader :key,
+  attr_reader :key_used,
               :date,
               :shifts
 
-  def initialize #(key, date)
-    @key = numbers.sample(5).to_s
+  def initialize
+    @key_used = nil
     @date = Date.today.strftime("%d%m%y")
   end
 
-  def shifts
-    keys = generate_keys
-    offsets = generate_offsets
-    #hash adding the keys of below
+  def numbers
+    Array.new(10) { |number| number }
+  end
+
+  def create_random_number
+    numbers.sample(5).join
   end
 
   def generate_keys
+    number = create_random_number
     key_collection = {
-                      a: @key.slice(0, 2),
-                      b: @key.slice(1, 2),
-                      c: @key.slice(2, 2),
-                      d: @key.slice(3, 2)
-                    }
-    key_collection.transform_values(&:to_i)
+                      a: number.slice(0, 2),
+                      b: number.slice(1, 2),
+                      c: number.slice(2, 2),
+                      d: number.slice(3, 2)
+                     }
+    freeze = key_collection.transform_values(&:to_i)
+    @key_used = freeze.values
+    freeze #this is silly but I want this RV
   end
 
   def generate_offsets
@@ -33,7 +38,7 @@ class Shift
                           c: offset.slice(2, 2),
                           d: offset.slice(3, 2)
                         }
-    offset_collection.transform_values(&:to_i)
+    freeze = offset_collection.transform_values(&:to_i)
   end
 
   def offset_helper
@@ -42,29 +47,16 @@ class Shift
     string.slice(-4, 4)
   end
 
-  def generate_shift
-    #something like:
-    shift = []
-    generate_keys.each do |key|
-      generate_offsets.each do |offset|
-        key + offset if key == offset
-      end
-    end
+  def shifts
+    keys = generate_keys
+    offsets = generate_offsets
+    {
+      a: keys[:a] + offsets[:a],
+      b: keys[:b] + offsets[:b],
+      c: keys[:c] + offsets[:c],
+      d: keys[:d] + offsets[:d]
+    }
   end
 
-  def numbers
-    Array.new(10) { |number| number }
-  end
+
 end
-
-
-
-# Consider the date formatted as a number, DDMMYY.
-# If the date is August 4, 1995, it would be represented
-# as 040895.
-# Square the numeric form (1672401025)
-# Take the last four digits (1025)
-# A offset: The first digit (1)
-# B offset: The second digit (0)
-# C offset: The third digit (2)
-# D offset: The fourth digit (5)
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -17,34 +17,34 @@ RSpec.describe Shift do
 
   it 'can create 5-digit key' do
     shift = Shift.new
-    require "pry"; binding.pry
-    shift.generate_keys
-    expect(shift.key.length).to eq(5)
+    expect(shift.create_random_number.length).to eq(5)
     # shift = double()
     # allow(shift).to receive(:key).and_return(02715) #is this how to stub?
-    # require "pry"; binding.pry
     # expect(shift.key).to eq(02715)
   end
 
-  xit 'has todays date' do
+  it 'has todays date' do
     allow(Date).to receive(:today).and_return(Date.new(2021,4,23))
     shift = Shift.new
     expect(shift.date).to eq("230421")
   end
 
-  xit 'generates keys' do
+  it 'generates keys' do
     shift = Shift.new
-    # # key1 = double() #stub
-    # # allow(key1).to receive(:key).and_return(02715)
-    # shift = double() #mock
-    # expect(shift).to receive(:generate_keys).with(02715)
-    # shift
+    # expect(shift.generate_keys).to be_an_instance_of(Hash)
+    # key1 = double() #stub
+    # allow(key1).to receive(:key).and_return(02715)
+    # shift = double()
+    # allow(shift).to receive(:create_random_number).with(32751)
+# require "pry"; binding.pry
     expect(shift.generate_keys).to be_an_instance_of(Hash)
-    key1 = double() #stub
-    allow(key1).to receive(:key).and_return(02715)
-    shift1 = double()
-    allow(shift1).to receive(:generate_keys).with(key1)
-    expected = {:a=>[0, 2], :b=>[2, 7], :c=>[7, 1], :d=>[1, 5]}
-    expect(shift1).to eq(expected)
+    # expected = {:a=>[0, 2], :b=>[2, 7], :c=>[7, 1], :d=>[1, 5]}
+    # expect(shift1).to eq(expected)
+  end
+
+  it 'stores key used in encryption' do
+    shift = Shift.new
+    expected = shift.generate_keys.values
+    expect(shift.key_used).to eq(expected)
   end
 end
